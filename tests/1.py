ca7cfa553bdf6cab25c2449886dccbc47a54b00b
kanl <106077541+kanl222@users.noreply.github.com>
2023-06-11 23:47:16 +0500

    Initial commit

---
+++ tests/1.py
@@ -0,0 +1,17 @@
+import random
+print(random.random())
+
+
+
+def isPalindrome(list_:list[int]):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        current_index = 0
+        last_index = 1
+        while current_index < last_index:
+                while list_[current_index] != list_[last_index]:
+                        last_index+=1
+                list_[current_index] =  
+        
